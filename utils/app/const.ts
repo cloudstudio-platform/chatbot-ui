7c9ace4bb59d500814c76e5d46891decc1845db2
huangkeyi <huangkeyi@coding.net>
2023-07-14 14:33:21 +0800

    修改 opanai 代理地址

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -3,7 +3,7 @@ export const DEFAULT_SYSTEM_PROMPT =
   "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
 
 export const OPENAI_API_HOST =
-  process.env.OPENAI_API_HOST || 'https://api.openai.com';
+  process.env.OPENAI_API_HOST || 'https://service-4y8atuq4-1259057771.sg.apigw.tencentcs.com';
 
 export const DEFAULT_TEMPERATURE = 
   parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "1");
